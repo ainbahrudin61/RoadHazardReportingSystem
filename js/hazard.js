86bbdb0e7e06f194513b0a3449bf268b4179923c
ainbahrudin61 <ainbahrudin61@gmail.com>
2026-07-13 15:49:42 +0800

    Enhance logout button functionality and improve sidebar styles

--- js/hazard.js
+++ js/hazard.js
@@ -8,6 +8,32 @@ import {
 
 } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-database.js";
 
+import { auth } from "./firebase-config.js";
+
+import { 
+    signOut 
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+
+const logoutBtn = document.getElementById("logoutBtn");
+
+
+logoutBtn.addEventListener("click",()=>{
+
+
+    signOut(auth)
+    .then(()=>{
+
+        alert("Logged out");
+
+        window.location.href="login.html";
+
+    });
+
+
+});
+
+
 // ===============================
 // Display Hazard Data
 // ===============================

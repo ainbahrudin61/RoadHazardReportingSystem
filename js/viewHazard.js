d7a23fb4fc6622f594692ce9dc7e06cd4a96e767
nik aina <2023657198@student.uitm.edu.my>
2026-07-15 01:45:40 +0800

    gabung

--- js/viewHazard.js
+++ js/viewHazard.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { auth, database, HAZARD_PATH } from "./firebase-config.js";
 
 import {
@@ -117,111 +116,10 @@ get(hazardRef)
     } else {
 
         photo.src = "../images/no-image.png";
-=======
-import { database } from "./firebase-config.js";
-
-
-import {
-    ref,
-    get,
-    child
-} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-database.js";
-
-
-
-// Get ID from URL
-
-const params = new URLSearchParams(window.location.search);
-
-const reportId = params.get("id");
-
-
-
-// Firebase reference
-
-const dbRef = ref(database);
-
-
-
-get(child(dbRef, "Reports/" + reportId))
-
-.then((snapshot)=>{
-
-
-    if(snapshot.exists()){
-
-
-        const data = snapshot.val();
-
-
-
-        document.getElementById("reportId").innerText = reportId;
-
-
-        document.getElementById("userName").innerText =
-        data.userName || "-";
-
-
-        document.getElementById("hazardType").innerText =
-        data.hazardType || "-";
-
-
-        document.getElementById("description").innerText =
-        data.description || "-";
-
-
-        document.getElementById("location").innerText =
-        data.location || "-";
-
-
-        document.getElementById("latitude").innerText =
-        data.latitude || "-";
-
-
-        document.getElementById("longitude").innerText =
-        data.longitude || "-";
-
-
-        document.getElementById("status").innerText =
-        data.status || "-";
-
-
-        document.getElementById("date").innerText =
-        data.date || "-";
-
-
-
-        if(data.image){
-
-
-            document.getElementById("reportImage").src =
-            data.image;
-
-
-        }
-        else{
-
-
-            document.getElementById("reportImage").style.display="none";
-
-
-        }
-
-
-
-    }
-
-    else{
-
-
-        alert("Report not found");
-
->>>>>>> aff937f5c3d43415dc1e69367f33f0aa5dc462f0
 
     }
 
 
-<<<<<<< HEAD
     // ======================
     // STATUS
     // ======================
@@ -265,15 +163,5 @@ get(child(dbRef, "Reports/" + reportId))
     console.error(error);
 
     alert("Failed to load hazard.");
-=======
-
-})
-
-.catch((error)=>{
-
-
-    console.error(error);
-
->>>>>>> aff937f5c3d43415dc1e69367f33f0aa5dc462f0
 
 });
